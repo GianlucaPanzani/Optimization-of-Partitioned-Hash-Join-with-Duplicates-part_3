6dbe4c825c95bd858837b4f545df063270dc8fb9
GianlucaPanzani <gianluca.panzani97@gmail.com>
2026-05-16 17:12:04 +0200

    execution of seq

--- src/runners/run_all.sh
+++ src/runners/run_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#./benchmark.sh hashjoin_seq grid/seq_grid_search.sh 5
+./benchmark.sh hashjoin_seq grid/seq_grid_search.sh 5
 
 #./benchmark.sh hashjoin_omp_loop grid/omp_loop_grid_search.sh 5
 #./benchmark.sh hashjoin_omp_loop_wb grid/omp_loop_grid_search.sh 5
@@ -11,6 +11,6 @@
 #./benchmark.sh hashjoin_omp_taskloop grid/omp_taskloop_grid_search.sh 5
 #./benchmark.sh hashjoin_omp_taskloop_wb grid/omp_taskloop_grid_search.sh 5
 
-./run_weak_scaling.sh
-./run_strong_scaling.sh
+#./run_weak_scaling.sh
+#./run_strong_scaling.sh
 
